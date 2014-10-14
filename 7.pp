74ba27f6e6e8b03f1459d4a3261032e386d5fbce
Joshua Hoblitt <josh@hoblitt.com>
2014-10-14 10:12:57 -0700

    add duplicate dep examples

--- 7.pp
+++ 7.pp
@@ -1,14 +1,19 @@
 notify { ['a', 'b', 'c', 'd']: }
 
 Notify['a','b'] -> Notify['c','d']
+Notify['a','b'] -> Notify['c','d']
 
+[ Notify['a'], Notify['b'] ] -> Notify['c','d']
 [ Notify['a'], Notify['b'] ] -> Notify['c','d']
 
 Notify['a','b'] -> [ Notify['c'], Notify['d'] ]
+Notify['a','b'] -> [ Notify['c'], Notify['d'] ]
 
+[ Notify['a'], Notify['b'] ] -> [ Notify['c'], Notify['d'] ]
 [ Notify['a'], Notify['b'] ] -> [ Notify['c'], Notify['d'] ]
 
 $foo = [ Notify['a'], Notify['b'] ] 
 $bar = [ Notify['c'], Notify['d'] ]
 
+$foo -> $bar
 $foo -> $bar
